5c90d974029add25fe25983060bd70ce05e1fdf4
Misir Jafarov <misir.ceferov@gmail.com>
2021-06-30 06:29:47 +0400

    Implemented runtime error handling

--- interpereter.go
+++ interpereter.go
@@ -20,12 +20,15 @@ func (i *Interpereter) visitBinaryExpr(expr *BinaryExpr) Any {
 
 	switch expr.operator.tokenType {
 	case MINUS:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) - right.(float)
 
 	case SLASH:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) / right.(float)
 
 	case STAR:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) * right.(float)
 
 	case PLUS:
@@ -34,34 +37,34 @@ func (i *Interpereter) visitBinaryExpr(expr *BinaryExpr) Any {
 			switch rightVal := right.(type) {
 			case float:
 				return leftVal + rightVal
-			case bool:
-				if rightVal {
-					return leftVal + 1
-				} else {
-					return leftVal
-				}
+			case string:
+				return fmt.Sprintf("%v%s", leftVal, rightVal)
 			}
 		case string:
 			switch rightVal := right.(type) {
 			case string:
 				return leftVal + rightVal
 			case float:
-			case bool:
 				return fmt.Sprintf("%s%v", leftVal, rightVal)
 			}
 		}
+		i.runtimeError(expr.operator, "Operands must be two numbers or two strings.")
 		break
 
 	case GREATER:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) > right.(float)
 
 	case GREATER_EQUAL:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) >= right.(float)
 
 	case LESS:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) < right.(float)
 
 	case LESS_EQUAL:
+		i.checkNumberOperands(expr.operator, left, right)
 		return left.(float) <= right.(float)
 
 	case EQUAL_EQUAL:
@@ -87,6 +90,7 @@ func (i *Interpereter) visitUnaryExpr(expr *UnaryExpr) Any {
 
 	switch expr.operator.tokenType {
 	case MINUS:
+		i.checkNumberOperand(expr.operator, right)
 		return -right.(float)
 	case BANG:
 		return !isTruthy(right)
@@ -113,4 +117,39 @@ func isEqual(a Any, b Any) bool {
 		return false
 	}
 	return a == b
+}
+
+func (i *Interpereter) checkNumberOperand(operator *Token, operand Any) {
+	switch operand.(type) {
+	case float:
+		return
+	}
+	i.runtimeError(operator, "Operand must be a number.")
+}
+
+func (i *Interpereter) checkNumberOperands(operator *Token, left Any, right Any) {
+	switch left.(type) {
+	case float:
+		switch right.(type) {
+		case float:
+			return
+		}
+	}
+	i.runtimeError(operator, "Operands must be a numbers.")
+}
+
+func (i *Interpereter) runtimeError(token *Token, message string) {
+	panic(RuntimeError{token: token, message: message})
+}
+
+type RuntimeError struct {
+	token   *Token
+	message string
+}
+
+func (e RuntimeError) Error() string {
+	if e.token == nil {
+		return "Runtime error: " + e.message
+	}
+	return fmt.Sprintf("Runtime error at line %v: %s", e.token.line, e.message)
 }
